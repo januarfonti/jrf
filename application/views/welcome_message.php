a3a2f29a3613b1688ca2297fcac172d4101f3cfc
Januar Fonti <me@januarfonti.com>
2016-10-26 19:31:09 +0700

    static view

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,77 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Januar Fonti</title>
+    <link href="https://fonts.googleapis.com/css?family=Cousine" rel="stylesheet">
 
-	<style type="text/css">
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url('dist')?>/css/bootstrap.css" rel="stylesheet">
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+    <link href="<?php echo base_url('dist')?>/css/style.css" rel="stylesheet">
+    <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
+<div class="container text-center">
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
+<div class="col-md-8 col-md-offset-2 result">
+    <div class="panel panel-default panel-transparent">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-md-4">
+                    <i class="ion-ios-circle-filled left"></i> <i class="ion-ios-circle-filled middle"></i> <i class="ion-ios-circle-filled right"></i>
+                </div>
+                <div class="col-md-4 text-center">
+                    <i class="ion-home"></i> kmzwa88saa - bash
+                </div>
+                <div class="col-md-4">
+                </div>
+            </div>
+
+
+        </div>
+        <div class="panel-body">
+            <p><span class="satu typed"></span></d>
+            <p><span class="dua typed"></span></p>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php echo base_url('dist')?>/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url('dist')?>/js/typed.js"></script>
+<script src="<?php echo base_url('dist')?>/js/jquery.backstretch.min.js"></script>
+<script>
+    $(function(){
+        $(".satu").typed({
+            strings: ["$: Halo,<br>^1000 $: Nama Saya Januar Fonti<br>^2000 $: Saya adalah seorang pesepeda<br>^2000 $: Selain itu saya juga seorang web developer<br>^2000 $: Ingin lebih mengenal dengan saya ?<br>^2000 $: Klik <a href='http://januarfonti.com' target='_blank'>tautan ini</a> untuk info lebih lanjut.<br>^2000 $: Bye ~ ^1000"],
+            contentType: 'html',
+            typeSpeed: 30,
+            showCursor: true,
+            loop:true,
+        });
+
+        $.backstretch("<?php echo base_url('dist'); ?>/img/bg.jpg");
+
+    });
+</script>
 </body>
 </html>
